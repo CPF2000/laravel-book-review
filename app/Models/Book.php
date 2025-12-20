f7e177d1a85aa5e281c38b61832a492452d263ea
chenpingfeng <717903589@qq.com>
2025-12-20 14:23:18 +0800

    重要:自定义scope范围查询函数

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Dotenv\Util\Str;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,5 +13,9 @@ class Book extends Model
     //设定跟reviews表的关系，1对多，所以函数名要加reviews不能是review
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+    //范围查询 必须以scope开头,直接在php artisan tinker中使用 App\Models\Book::title('doloremque')->get();
+    public function scopeTitle(Builder $query,String $title):Builder{
+        return $query->where('title', 'LIKE', "%$title%");
     }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+    protected $fillable = ['review',  'rating'];//设置fillable属性，允许使用$book->reviews()->create(['review' => '太好了', 'rating' => 3])方法添加数据
+
     //设置跟books数据表的关联关系，多个reviews对应一个 book，函数名为单数book，不能为books
     public function book()
     {
