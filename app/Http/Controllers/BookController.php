a5895fb10b5e9cd0e403e4c45839efadc505a845
chenpingfeng <717903589@qq.com>
2025-12-23 22:53:50 +0800

    增加Cache缓存

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
@@ -12,6 +13,22 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input('title');
+        $filter = $request->input('filter', '');//没有指定数值，默认为''
+        //when方法的第一个参数若为空则不会执行function，而是直接执行get()获取所有数据
+        $books = Book::when($title, function ($query, $title) {
+            return $query->title($title);//这边的title方法是引用自Book模型的scopeTitle方法
+        });
+
+        $books = match ($filter) {
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $books = $books->get();
+        return view('books.index', ["books" => $books]);
+        //return view('books.index', compact('books'));//compact('books')将会把$books变量传递给视图
     }
 
     /**
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,36 +11,57 @@ class Book extends Model
 {
     use HasFactory;
     //设定跟reviews表的关系，1对多，所以函数名要加reviews不能是review
-    public function reviews(){
+    public function reviews()
+    {
         return $this->hasMany(Review::class);
     }
     //范围查询 必须以scope开头,直接在php artisan tinker中使用 App\Models\Book::title('doloremque')->get();
-    public function scopeTitle(Builder $query,String $title):Builder{
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
         return $query->where('title', 'LIKE', "%$title%");
     }
     //过滤评论比较多的书
-    public function scopePopular(Builder $query,$from=null,$to=null):Builder | QueryBuilder{
-       return $query->withCount(['reviews'=>fn($q)=>$this->dateRangeFilter($q,$from,$to)])->orderBy('reviews_count', 'desc');
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withCount(['reviews' => fn($q) => $this->dateRangeFilter($q, $from, $to)])->orderBy('reviews_count', 'desc');
     }
     //过滤得分最高的书
-    public function scopeHighestRated(Builder $query,$from=null,$to=null):Builder | QueryBuilder{
-        return $query->withAvg(['reviews'=>fn($q)=>$this->dateRangeFilter($q,$from,$to)], 'rating')->orderBy('reviews_avg_rating', 'desc');
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
+    {
+        return $query->withAvg(['reviews' => fn($q) => $this->dateRangeFilter($q, $from, $to)], 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
-    public function scopeMinReviews(Builder $query,int $minReviews):Builder | QueryBuilder{
-        return $query->having('reviews_count','>=',$minReviews);
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
     }
 
-
-    private function dateRangeFilter(Builder $query,$from=null,$to=null){
-        if($from && !$to){
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
             $query->where('created_at', '>=', $from);
-        }elseif(!$from && $to){
+        } elseif (!$from && $to) {
             $query->where('created_at', '<=', $to);
-        }elseif($from && $to){
+        } elseif ($from && $to) {
             $query->whereBetween('created_at', [$from, $to]);
         }
     }
 
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())->highestRated(now()->subMonth(), now())->minReviews(2);
+    }
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(6), now())->highestRated(now()->subMonth(6), now())->minReviews(5);
+    }
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())->popular(now()->subMonth(), now())->minReviews(2);
+    }
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(6), now())->popular(now()->subMonth(6), now())->minReviews(5);
+    }
 
 
 }
